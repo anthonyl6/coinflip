0fb7b942fb401e3c0e9aaf47992135b99f3bb4ac
anthonyl6 <anthonyl6@learn.cssd.ab.ca>
2018-09-20 14:33:47 -0600

    Finished the coin flip and aded comments.

--- coinflip/Program.cs
+++ coinflip/Program.cs
@@ -10,7 +10,34 @@ namespace coinflip
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int coin;
+            string userGuess;
+            string coinDescription = "Tails";
+            Random rng = new Random();
+
+            //Query the user
+            Console.Write("Enter your guess Heads or Tails (H or T)");
+            userGuess = Console.ReadLine();
+
+            //Get a random number for the coin flip
+            coin = rng.Next(0, 2);
+            if(coin == 1) {
+
+                coinDescription = "Heads";
+
+            }
+
+            if((coin == 0 && userGuess == "T") || (coin == 1 && userGuess == "H")) {
+
+                Console.WriteLine("The coin flip was {0}, you win!", coinDescription);
+
+            } else {
+
+                Console.WriteLine("The coin flip was {0}, you lose!", coinDescription);
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
